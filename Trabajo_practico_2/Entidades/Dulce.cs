eced267ec45b1704bcccd99090151b19989b8b73
Emiliano Medina <a>
2019-05-19 22:56:16 -0300

    Trabajo practico 2

---
+++ Trabajo_practico_2/Entidades/Dulce.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Dulce : Producto
+    {
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+        }
+
+        /// <summary>
+        /// Constructor de Dulce
+        /// </summary>
+        /// <param name="codigo">Codigo de barras</param>
+        /// <param name="marca">Marca del producto</param>
+        /// <param name="color">Color primario del empaque</param>
+        public Dulce(EMarca marca, string codigo, ConsoleColor color) : base(codigo, marca, color)
+        {
+        }
+
+        /// <summary>
+        /// Devuelve un string con los datos del dulce
+        /// </summary>
+        /// <returns></returns>
+        public sealed override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Trabajo_practico_2/Entidades/Snacks.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Snacks : Producto
+    {
+        
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 104;
+            }
+        }
+
+        /// <summary>
+        /// Constructor de Snacks
+        /// </summary>
+        /// <param name="codigo">Codigo de barras</param>
+        /// <param name="marca">Marca del producto</param>
+        /// <param name="color">Color primario del empaque</param>
+        public Snacks(EMarca marca, string codigo, ConsoleColor color)
+            : base(codigo, marca, color)
+        {
+        }
+
+        /// <summary>
+        /// Devuelve un string con los datos del snack
+        /// </summary>
+        /// <returns></returns>
+        public sealed override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
